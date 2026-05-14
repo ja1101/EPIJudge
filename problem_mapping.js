b2d550ff6e1cd4dbac6ac0b54279f9bbed62fd39
Jose Alvarado <jose.alvarado@uopeople.edu>
2026-05-14 16:55:49 -0700

    anagrams solution

--- problem_mapping.js
+++ problem_mapping.js
@@ -65,7 +65,7 @@ problem_mapping = {
                 "passed": 0,
                 "total": 10000
             },
-            "Python: closest_int_same_weight.py": { 
+            "Python: closest_int_same_weight.py": {
                 "passed": 0,
                 "total": 10000
             }
@@ -1442,7 +1442,7 @@ problem_mapping = {
     "Chapter 12: Hash Tables": {
         "12.00 Bootcamp: Hash Tables": {
             "C++: anagrams.cc": {
-                "passed": 0,
+                "passed": 9,
                 "total": 9
             },
             "Java: Anagrams.java": {
